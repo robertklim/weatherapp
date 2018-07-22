c90164ba006b4c9c7d48e2d0adcd81ede326a38c
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-22 15:11:39 +0200

    Pull data for one city from api.openweathermap.org and display it on the front page

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,4 +1,21 @@
+import requests
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'weather/weather.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=dd9f63d91bf3db2c048962dbaef3ac94'
+    city = 'Warsaw'
+
+    res = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city': city,
+        'temperature': res['main']['temp'],
+        'description': res['weather'][0]['description'],
+        'icon': res['weather'][0]['icon'],
+    }
+
+    context = {
+        'city_weather': city_weather
+    }
+
+    return render(request, 'weather/weather.html', context)

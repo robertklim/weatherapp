9922a097d1ea976298d028f21bbeddac04a6fb99
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-23 08:28:20 +0200

    Load cieties from database, get their weather and display all data on the front page

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,21 +1,30 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 def home(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=dd9f63d91bf3db2c048962dbaef3ac94'
     city = 'Warsaw'
 
-    res = requests.get(url.format(city)).json()
+    cities = City.objects.all().order_by('name')
 
-    city_weather = {
-        'city': city,
-        'temperature': res['main']['temp'],
-        'description': res['weather'][0]['description'],
-        'icon': res['weather'][0]['icon'],
-    }
+    weather_data = []
+
+    for city in cities:
+
+        res = requests.get(url.format(city.name)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': res['main']['temp'],
+            'description': res['weather'][0]['description'],
+            'icon': res['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
 
     context = {
-        'city_weather': city_weather
+        'weather_data': weather_data
     }
 
     return render(request, 'weather/weather.html', context)
